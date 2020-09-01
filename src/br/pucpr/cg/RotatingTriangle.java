6f52ffc9b4aa9eaefac7136ce1ecdb4c50160240
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2020-09-01 18:52:46 -0300

    Updated to Java 10, LWJGL 3.2.3 and JOML1.9.25

--- src/br/pucpr/cg/RotatingTriangle.java
+++ src/br/pucpr/cg/RotatingTriangle.java
@@ -11,7 +11,7 @@ import br.pucpr.mage.Scene;
 import br.pucpr.mage.Shader;
 import br.pucpr.mage.Window;
 import org.joml.Matrix4f;
-import org.lwjgl.BufferUtils;
+import org.lwjgl.system.MemoryStack;
 
 import java.nio.FloatBuffer;
 
@@ -24,14 +24,6 @@ public class RotatingTriangle implements Scene {
 	/** Esta variável guarda o identificador da malha (Vertex Array Object) do triângulo */
 	private int vao;
 
-
-	/** Guarda o id do buffer com todas as posições do vértice. */
-	private int positions;
-
-	/** Guarda o id do buffer com todas as cores do vértice */
-	private int colors;
-
-
 	/** Guarda o id do shader program, após compilado e linkado */
 	private int shader;
 
@@ -42,6 +34,11 @@ public class RotatingTriangle implements Scene {
 	public void init() {
 		//Define a cor de limpeza da tela
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+		//------------------------------
+		//Carga/Compilação dos shaders
+		//------------------------------
+		shader = Shader.loadProgram("basic");
 
 		//------------------
 		//Criação da malha
@@ -56,54 +53,64 @@ public class RotatingTriangle implements Scene {
 		//Informamos a OpenGL que iremos trabalhar com esse VAO
 		glBindVertexArray(vao);
 
-
 		//Criação do buffer de posições
 		//------------------------------
-		float[] vertexData = new float[] {
+		var vertexData = new float[] {
 				0.0f,  0.5f,   //Vértice 0
 				-0.5f, -0.5f,  //Vértice 1
 				0.5f, -0.5f    //Vértice 2
 		};
 
 		//Solicitamos a criação de um buffer na OpenGL, onde esse array será guardado
-		positions = glGenBuffers();
+		var positions = glGenBuffers();
 		//Informamos a OpenGL que iremos trabalhar com esse buffer
 		glBindBuffer(GL_ARRAY_BUFFER, positions);
 
 		//Damos o comando para carregar esses dados na placa de vídeo
 		glBufferData(GL_ARRAY_BUFFER, vertexData, GL_STATIC_DRAW);
 
-		//Como já finalizamos a carga, informamos a OpenGL que não estamos mais usando esse buffer.
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		//Procuramos o identificador do atributo de posição
+		var aPosition = glGetAttribLocation(shader, "aPosition");
+
+		//Chamamos uma função que associa a posição ao buffer.
+		glVertexAttribPointer(aPosition, 2, GL_FLOAT, false, 0, 0);
 
 		//Criação do buffer de cores
 		//------------------------------
-		float[] colorData = new float[] {
+		var colorData = new float[] {
 				1.0f,  0.0f, 0.0f,  //Vértice 0
 				0.0f,  1.0f, 0.0f,  //Vértice 1
 				0.0f,  0.0f, 1.0f,  //Vértice 2
 		};
 
 		//Solicitamos a criação de um buffer na OpenGL, onde esse array será guardado
-		colors = glGenBuffers();
+		var colors = glGenBuffers();
 		//Informamos a OpenGL que iremos trabalhar com esse buffer
 		glBindBuffer(GL_ARRAY_BUFFER, colors);
 
 		//Damos o comando para carregar esses dados na placa de vídeo
 		glBufferData(GL_ARRAY_BUFFER, colorData, GL_STATIC_DRAW);
 
+		//Informamos a OpenGL que iremos trabalhar com essa variável
+		glEnableVertexAttribArray(aPosition);
+
+		//Associação do buffer cores a variável aColor
+		//---------------------------------------------------
+		//Procuramos o identificador do atributo de posição
+		var aColor = glGetAttribLocation(shader, "aColor");
+
+		//Chamamos uma função que associa a cor ao shader.
+		// Observe que o size mudou para 3, já que as cores são um vec3 com 3 floats para os componentes r, g e b
+		glVertexAttribPointer(aColor, 3, GL_FLOAT, false, 0, 0);
+
+		//Informamos a OpenGL que iremos trabalhar com essa variável
+		glEnableVertexAttribArray(aColor);
+
 		//Como já finalizamos a carga, informamos a OpenGL que não estamos mais usando esse buffer.
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-
 		//Finalizamos o nosso VAO, portanto, informamos a OpenGL que não iremos mais trabalhar com ele
 		glBindVertexArray(0);
-
-		//------------------------------
-		//Carga/Compilação dos shaders
-		//------------------------------
-
-		shader = Shader.loadProgram("basic.vert", "basic.frag");
 	}
 
 	@Override
@@ -130,60 +137,25 @@ public class RotatingTriangle implements Scene {
 		//E qual shader program irá ser usado durante o desenho
 		glUseProgram(shader);
 
-
 		//Associação da variável World ao shader
 		//--------------------------------------
-		//Criamos um objeto da classe FloatBuffer
-		FloatBuffer transform = BufferUtils.createFloatBuffer(16);
+		try (MemoryStack stack = MemoryStack.stackPush()) {
+			//Criamos uma matriz de rotação e a enviamos para o buffer transform
+			FloatBuffer transform = new Matrix4f()
+					.rotateY(angle)
+					.get(stack.mallocFloat(16));
 
-		//Criamos uma matriz de rotação e a enviamos para o buffer transform
-		new Matrix4f().rotateY(angle).get(transform);
+			//Procuramos pelo id da variável uWorld, dentro do shader
+			var uWorld = glGetUniformLocation(shader, "uWorld");
 
-		//Procuramos pelo id da variável uWorld, dentro do shader
-		int uWorld = glGetUniformLocation(shader, "uWorld");
-
-		// Copiamos os dados do buffer para a variável que está no shader
-		glUniformMatrix4fv(uWorld, false, transform);
-
-
-		//Associação do buffer positions a variável aPosition
-		//---------------------------------------------------
-		//Procuramos o identificador do atributo de posição
-		int aPosition = glGetAttribLocation(shader, "aPosition");
-
-		//Informamos a OpenGL que iremos trabalhar com essa variável
-		glEnableVertexAttribArray(aPosition);
-
-		//Informamos ao OpenGL que também trabalharemos com o buffer de posições
-		glBindBuffer(GL_ARRAY_BUFFER, positions);
-
-		//Chamamos uma função que associa as duas.
-		glVertexAttribPointer(aPosition, 2, GL_FLOAT, false, 0, 0);
-
-
-		//Associação do buffer cores a variável aColor
-		//---------------------------------------------------
-		//Procuramos o identificador do atributo de posição
-		int aColor = glGetAttribLocation(shader, "aColor");
-
-		//Informamos a OpenGL que iremos trabalhar com essa variável
-		glEnableVertexAttribArray(aColor);
-
-		//Informamos ao OpenGL que também trabalharemos com o buffer de cores
-		glBindBuffer(GL_ARRAY_BUFFER, colors);
-
-		//Chamamos uma função que associa as duas. Observe que o size mudou para 3, já que as cores são um vec3 com 3
-		// floats para os componentes r, g e b
-		glVertexAttribPointer(aColor, 3, GL_FLOAT, false, 0, 0);
-
+			// Copiamos os dados do buffer para a variável que está no shader
+			glUniformMatrix4fv(uWorld, false, transform);
+		}
 
 		//Comandamos o desenho de 3 vértices
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		//Faxina
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glDisableVertexAttribArray(aPosition);
-        glDisableVertexAttribArray(aColor);
 		glBindVertexArray(0);
 		glUseProgram(0);
 	}
